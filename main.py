a40580698138954aea9111cfb23b26e2090e77ea
Dylan Buchi <devybuchi@gmail.com>
2020-09-25 06:39:56 -0300

    finish script

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import hashlib
-
+import sys
 from requests.models import Response
 
 
@@ -21,14 +21,24 @@ def check_pwned_api(password: str):
     sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first_five_characters, remaining = sha1_password[:5], sha1_password[5:]
     response = data_request_pwned_api(first_five_characters)
-
     return get_count_from_password_leaks(response, remaining)
 
 
 def get_count_from_password_leaks(hash: Response, hash_to_check: str):
-    hashes_and_counts = (line.split(':') for line in hash.text.splitlines())
-    print([(h, c) for h, c in hashes_and_counts])
+    lst = (line.split(':') for line in hash.text.splitlines())
+    for h, count in lst:
+        if h == hash_to_check:
+            return count or 0
+
+
+def main(args: list = ["python"]):
+    password = args[0]
+    count = check_pwned_api(password)
+    print(f"Good news, the password \"{password}\" was not found."
+          ) if count is None else print(
+              f"Oh no... the password \"{password}\" was found {count} times")
 
 
 if __name__ == "__main__":
-    response = check_pwned_api("ahaha")
+    password = sys.argv[1:2]
+    main() if len(password) == 0 else main(password)
